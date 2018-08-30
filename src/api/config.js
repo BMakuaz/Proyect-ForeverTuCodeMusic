1e8d1d3075363e8f480d08cb0ee984f17143fa1a
Marko <markoz@Bar.com>
2018-08-30 02:59:45 -0500

    Final proyecto

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+  appName: 'forevertucodeMusic',
+  apiKey: 'cc9abf617294c06f514aa3cc73d44d50',
+  secret: '1b02a91c5e52a88884b1b8ac71c316ba',
+  RegisteredTo: ' Barrera-zanta'
+}
+
+/*Application name    forevertucodeMusic
+API key cc9abf617294c06f514aa3cc73d44d50
+Shared secret   1b02a91c5e52a88884b1b8ac71c316ba
+Registered to   Barrera-zanta*/
